80f29b5c2ec3ba299b0c2875ab94f5bc09f7afce
torresjo2396 <torresjo2396@gmail.com>
2025-05-28 12:17:29 -0600

    Se hicieron pruebas usando colas

--- colas/Program.cs
+++ colas/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace colas
+﻿using System.Collections;
+
+namespace colas
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World subido!");
+            Queue myQueue = new Queue();
+
+            myQueue.Enqueue("First Element");
+            myQueue.Enqueue("Second Element");
+            myQueue.Enqueue("Third Element");
+            myQueue.Enqueue("Fourth Element");
+
+            Queue myQueueClone =  (Queue)myQueue.Clone();
+
+            Console.WriteLine("Queue size: " + myQueue.Count);
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Element " + (i + 1) + ": " + myQueue.Dequeue());
+            }
+
+            Console.WriteLine("Queue size after dequeue: " + myQueue.Count);
         }
     }
 }
